e3cdfa8f839f88830f83d1856dba650beb64ec8d
Théophile Walter <theophilewalter@gmail.com>
2016-03-18 10:22:20 +0100

    Started, implementation

--- ProjetML/src/image/ImageDisplay.java
+++ ProjetML/src/image/ImageDisplay.java
@@ -1,13 +1,45 @@
-/**
+/*
  *  Class ImageDisplay
  *
  *  Displays Image objects.
  *
- *  @author Jules Pénuchot
+ *  @author Jules Pénuchot, Théophile Walter
  */
 
 package image;
 
-public class ImageDisplay{
-  Image displayImage;
+import java.awt.*; 
+import javax.swing.*; 
+
+public class ImageDisplay extends JFrame {
+	
+	private Image displayImage = null;
+	private boolean displayed = false;
+	private JPanel panel = new JPanel();
+	
+	/**
+	 * Constructor with an image
+	 */
+	public ImageDisplay ( Image img) {
+		displayImage = img;
+	}
+	
+	/**
+	 * Constructor without image
+	 */
+	public ImageDisplay () { }
+	
+	/**
+	 * Display the window
+	 */
+	public void show () {
+		if (displayImage == null) {
+			System.out.println("ImageDisplay.java: Error, no image given in constructor!");
+			return;
+		}
+		/**
+		 * TODO: Display the image to JFrame
+		 */
+	}
+  
 }
