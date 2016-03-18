98a041158a58c5ee0666ef8b08e3d6ee76841d27
Théophile Walter <theophilewalter@gmail.com>
2016-03-18 10:19:00 +0100

    ImageDB is now iteratable

--- ProjetML/src/image/ImageDB.java
+++ ProjetML/src/image/ImageDB.java
@@ -2,6 +2,7 @@
  * Class ImageDB
  * 
  * Represents a standardized set of Images.
+ * Iteratable.
  * 
  * @author Jules Pénuchot, Théophile Walter
  */
@@ -9,45 +10,83 @@
 package image;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 import image.Image;
 
-public class ImageDB {
-	
+public class ImageDB implements Iterable<Image>, Iterator<Image> {
+
 	private ArrayList<Image> imDB;
-	
+	private int count = 0;
+
 	/**
 	 * Create the database from an ArrayList
 	 */
-	public ImageDB ( ArrayList<Image> db) {
+	public ImageDB(ArrayList<Image> db) {
 		imDB = db;
 	}
-	
+
 	/**
 	 * Create a void database
 	 */
-	public ImageDB () {
-		imDB = new ArrayList<Image> ();
+	public ImageDB() {
+		imDB = new ArrayList<Image>();
 	}
-	
+
 	/**
 	 * Add an image to the database
 	 */
-	public void add ( Image img) {
+	public void add(Image img) {
 		imDB.add(img);
 	}
-	
+
 	/**
 	 * Remove an image from the database
 	 */
-	public Image remove ( int index) {
+	public Image remove(int index) {
 		return imDB.remove(index);
 	}
-	
+
 	/**
 	 * Return the size of the database
 	 */
-	public int size () {
+	public int size() {
 		return imDB.size();
 	}
 	
+	/**
+	 * Return the image at index position
+	 */
+	public Image get(int index) {
+		return imDB.get(index);
+	}
+
+	/**
+	 * Methods for iteration
+	 */
+	@Override
+	public boolean hasNext() {
+		if (count < imDB.size()) return true;
+		return false;
+	}
+
+	@Override
+	public Image next() {
+		if (count == imDB.size())
+			throw new NoSuchElementException();
+		count++;
+		return imDB.get(count);
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Iterator<Image> iterator() {
+		return this;
+	}
+
 }
