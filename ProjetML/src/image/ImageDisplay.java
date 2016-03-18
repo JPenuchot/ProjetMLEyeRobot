842e3220ebc3aabfbd877884fb2fb7d7b632fabd
Théophile Walter <theophilewalter@gmail.com>
2016-03-18 22:23:28 +0100

    Finished implementation of IlageDisplay.java

--- ProjetML/src/image/ImageDisplay.java
+++ ProjetML/src/image/ImageDisplay.java
@@ -8,38 +8,63 @@
 
 package image;
 
-import java.awt.*; 
-import javax.swing.*; 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
 
 public class ImageDisplay extends JFrame {
 	
-	private Image displayImage = null;
-	private boolean displayed = false;
-	private JPanel panel = new JPanel();
+	private Image displayImage;
+	private static final int SIZE = 10;
+	private boolean currentState = false;
 	
 	/**
-	 * Constructor with an image
+	 * Constructor
 	 */
 	public ImageDisplay ( Image img) {
 		displayImage = img;
+		this.setPreferredSize(new Dimension(img.getWidth()*SIZE, img.getHeight()*SIZE));
+		this.pack();
 	}
 	
 	/**
-	 * Constructor without image
+	 * Display/hide the window
 	 */
-	public ImageDisplay () { }
+	public void setVisible (boolean state) {
+		super.setVisible(state);
+		currentState = state;
+	}
 	
 	/**
-	 * Display the window
+	 * Change the image
 	 */
-	public void show () {
-		if (displayImage == null) {
-			System.out.println("ImageDisplay.java: Error, no image given in constructor!");
-			return;
+	public void setImage (Image img) {
+		displayImage = img;
+		if (currentState) {
+			super.repaint(); // Redraw the window if it is currently displayed
 		}
-		/**
-		 * TODO: Display the image to JFrame
-		 */
+	}
+	
+	/**
+	 * This method must be reimplemented to modify the drawing of the window
+	 */
+	@Override
+	public void paint(Graphics g) {
+		
+		// Call the initial method
+		super.paint(g);
+		
+		// Draw the image
+		char[] pixels = displayImage.getBuffer();
+		int size = pixels.length/3;
+		for (int i = 0; i < size; i++) {
+			g.setColor(new Color(pixels[i*3], pixels[i*3 + 1], pixels[i*3 + 2]));
+			int x = i % displayImage.getWidth(), y = (i - x) / displayImage.getWidth();
+			g.fillRect(x*SIZE, y*SIZE, SIZE, SIZE);
+		}
+		
 	}
   
 }
