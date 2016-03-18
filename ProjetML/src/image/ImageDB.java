7476aff1f2a5262f2bdc550fd3dcaccea31d0cc7
Théophile Walter <theophilewalter@gmail.com>
2016-03-18 09:34:53 +0100

    Implemented ImageDB.java

--- ProjetML/src/image/ImageDB.java
+++ ProjetML/src/image/ImageDB.java
@@ -12,5 +12,42 @@ import java.util.ArrayList;
 import image.Image;
 
 public class ImageDB {
-	ArrayList<Image> imDB;
+	
+	private ArrayList<Image> imDB;
+	
+	/*
+	 * Create the database from an ArrayList
+	 */
+	public ImageDB ( ArrayList<Image> db) {
+		imDB = db;
+	}
+	
+	/*
+	 * Create a void database
+	 */
+	public ImageDB () {
+		imDB = new ArrayList<Image> ();
+	}
+	
+	/*
+	 * Add an image to the database
+	 */
+	public void add ( Image img) {
+		imDB.add(img);
+	}
+	
+	/*
+	 * Remove an image from the database
+	 */
+	public Image remove ( int index) {
+		return imDB.remove(index);
+	}
+	
+	/*
+	 * Return the size of the database
+	 */
+	public int size () {
+		return imDB.size();
+	}
+	
 }
