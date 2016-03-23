e253411025e2cdb1a34beefaa6b140e385e927ea
Théophile Walter <theophilewalter@gmail.com>
2016-03-23 22:18:50 +0100

    Updated tests according to the update of Image class

--- ProjetML/src/image/ImageTest.java
+++ ProjetML/src/image/ImageTest.java
@@ -18,20 +18,13 @@ public class ImageTest {
 		// Default test image (3x2 French flag)
 		char[] imgChar = {0, 0, 255, 255, 255, 255, 255, 0, 0,
 						0, 0, 255, 255, 255, 255, 255, 0, 0};
-		String imgCSV = "0,255,255,0,255,255," +
-						"0,255,0,0,255,0," +
-						"255,255,0,255,255,0";
 		
 		Image testChar = new Image ( imgChar, 3, 2);
-		Image testCSV = new Image ( imgCSV, 3, 2);
 		
 		// getBuffer()
 		char[] bufferChar = testChar.getBuffer();
-		char[] bufferCSV = testCSV.getBuffer();
-		assertEquals(bufferChar.length, bufferCSV.length);
 		for (int i = 0; i < bufferChar.length; i++) {
 			assertEquals(bufferChar[i], imgChar[i]);
-			assertEquals(bufferCSV[i], imgChar[i]);
 		}
 		
 		// setPixel()
@@ -39,30 +32,17 @@ public class ImageTest {
 		testChar.setPixel(2, 0, Image.G, (char)255);
 		assertEquals(testChar.getBuffer()[7], 255);
 		
-		assertEquals(testCSV.getBuffer()[7], 0);
-		testCSV.setPixel(2, 0, Image.G, (char)255);
-		assertEquals(testCSV.getBuffer()[7], 255);
-		
 		// getPixel()
 		assertEquals(testChar.getPixel(2, 0, Image.G), 255);
-
-		assertEquals(testCSV.getPixel(2, 0, Image.G), 255);
 		
 		// getLabel() / setLabel()
 		assertEquals(testChar.getLabel(), 0);
 		testChar.setLabel(42);
 		assertEquals(testChar.getLabel(), 42);
-
-		assertEquals(testCSV.getLabel(), 0);
-		testCSV.setLabel(42);
-		assertEquals(testCSV.getLabel(), 42);
 		
 		// getWidth() / getHeight
 		assertEquals(testChar.getWidth(), 3);
 		assertEquals(testChar.getHeight(), 2);
-
-		assertEquals(testCSV.getWidth(), 3);
-		assertEquals(testCSV.getHeight(), 2);
 		
 	}
 
