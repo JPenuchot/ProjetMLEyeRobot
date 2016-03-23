e6fbdd194ac50746d3b7cfe74d3bf0afb6d1efc5
Jules Pénuchot <jules.penuchot@u-psud.fr>
2016-03-23 17:49:46 +0100

    Added base statistics classes

---
+++ ProjetML/src/statistics/DBStats.java
@@ -0,0 +1,13 @@
+package statistics;
+
+import image.*;
+
+public class DBStats {
+	ImageDB db;
+	
+	public DBStats(ImageDB _db){
+		db = _db;
+	}
+	
+	
+}
---
+++ ProjetML/src/statistics/ImageStats.java
@@ -0,0 +1,35 @@
+package statistics;
+
+import image.*;
+
+public class ImageStats {
+	private Image img;
+	
+	public ImageStats(Image _img){
+		img = _img;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public char[] verticalHistogram(){
+		return null;
+	}
+	
+	/**
+	 * Computes the horizontal histogram.
+	 * @return horizontal histogram
+	 */
+	public char[] horizontalHistogram(){
+		return null;
+	}
+	
+	public double sparsity(){
+		return null;
+	}
+	
+	public int label(){
+		return null;
+	}
+}
