eda886cb87d4ad772e8a7e5a1c5c7f3f236f344b
Théophile Walter <theophilewalter@gmail.com>
2016-04-03 21:40:18 +0200

    Update csvio.py

--- ProjetML/src/scripts/csvio.py
+++ ProjetML/src/scripts/csvio.py
@@ -3,17 +3,27 @@ import csv
 # Read CSV and return an list of list of integer values
 def readCSV(path):
     file = open(path, "r")
-    return csv.reader(file)
+    result = []
+    i = 0
+    for row in csv.reader(file):
+        i += 1
+        if i == 1: # 1st line is not an image
+            continue
+        sub = []
+        for el in row:
+            sub.append(int(el))
+        result.append(sub)
+    return result
 # End
 
 # Print a list
-def print_list2d (l):
+def print_list2d_type (l):
     i = 0
     for e in l:
         print ("l[" + str(i) + "] len : " + str(len(e)))
         i += 1
         for s in e:
-            print (str(s) + ", ", end = "")
+            print (str(type(s)) + ", ", end = "")
 # End
 
 def tab_to_xy(tab):
@@ -23,3 +33,7 @@ def tab_to_xy(tab):
 def save_labels(ytab):
     return
 # End
+
+#data = readCSV("C:\\Users\\Théophile\\Java\\Projet ML\\data\\local_train.csv")
+#print ("Reading done.")
+#print_list2d_type (data)
