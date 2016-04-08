142b3952dbfdb64b8f5161524d118d31dde10a7c
Théophile Walter <theophilewalter@gmail.com>
2016-04-08 17:09:42 +0200

    Update ImageStatsTest.java

--- ProjetML/src/statistics/ImageStatsTest.java
+++ ProjetML/src/statistics/ImageStatsTest.java
@@ -28,30 +28,3 @@ public class ImageStatsTest {
 	}
 
 }
-
-//////////////////////////////////////////////////////////////////////////////
-
-package statistics;
-
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-
-import org.junit.Test;
-
-import image.ImageDB;
-import io.CSVContiguous;
-
-public class DBStatsTest {
-
-	@Test
-	public void test() throws IOException {
-		
-		ImageDB db = (new CSVContiguous()).readDB("test.csv");
-		
-		int[] lab = {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-		
-		assertEquals(DBStats.sparsity(db), 64./1024., 0.01);
-		assertArrayEquals(DBStats.labelEffectives(db), lab);
-	}
-}
